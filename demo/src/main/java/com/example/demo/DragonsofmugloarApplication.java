39f503ade63be56d04df4c035da95ad3c5d49d56
Kennu76 <kevin.kanarbik@gmail.com>
2021-01-28 16:52:14 +0200

    initial commit

---
+++ demo/src/main/java/com/example/demo/DragonsofmugloarApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonsofmugloarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DragonsofmugloarApplication.class, args);
+	}
+
+}
